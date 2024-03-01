1c9571062557b80499fca1dcf8f4fb76de73372f
GerlandoMarsala <gerlando.marsala@outlook.fr>
2024-03-01 16:45:36 +0100

    refonte de la page contact desktop et responsive, breakpoints mis en place pour le responsive

---
+++ codePoubelle.php
@@ -0,0 +1,42 @@
+<div class="containerInfosAndForm">
+        <!-- <div class="containerLeftContact text-center">
+            <p>BC BEHREN,<br>COSEC, 15 Rue du Stade,<br>
+                57460 Behren-lès-Forbach<br>
+                M. Christian PIGA<br>
+                Président du BC Behren<br>
+                Tél : 06 XX XX XX XX <br>
+                Email : pigahabitat@gmail.com</p>
+            SUIVEZ NOUS <br>
+            <i class="fa-brands fa-facebook"></i>
+        </div>
+        <div class="containerRightContact">
+            <form data-aos="zoom-in" method="post" action="../controller/traitement_contact.php">
+            
+            <div class="form-floating mb-3">
+                    <input type="text" class="form-control" style="width:500px;" name="objet" id="floatingInputDisabled" placeholder="Objet" disable>
+                    <label for="floatingInputDisabled">Objet :</label>
+                    
+                </div>    
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" name="nom" id="floatingInputDisabled" placeholder="Nom" disable>
+                    <label for="floatingInputDisabled">Nom :</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" name="prenom" id="floatingInputDisabled" placeholder="Prénom" disable>
+                    <label for="floatingInputDisabled">Prénom :</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" name="email" placeholder="name@example.com" id="floatingTextareaDisabled" disable></input>
+                    <label for="floatingTextareaDisabled">Email :</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <textarea class="form-control" name="msg" placeholder="Ecrire votre message ici ..." id="floatingTextarea2Disabled" style="height: 100px" disable></textarea>
+                    <label for="floatingTextarea2Disabled">Message :</label>
+                </div>
+                <input type="submit" value="Envoyer" class="btn btn-dark buttonEnvoyer"></input>
+            </form>
+        </div> -->
+
+        
+        
+    </div>
